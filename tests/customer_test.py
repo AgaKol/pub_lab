56e2dad2ac4337f4c569bc5fcd117f2e398364e8
Aga Koltun <agnieszka.koltun.89@gmail.com>
2021-10-27 11:58:29 +0100

    test_customer_has_name added

--- tests/customer_test.py
+++ tests/customer_test.py
@@ -3,4 +3,8 @@ from src.customer import Customer
 
 
 class CustomerTest(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.customer = Customer("Frodo", 51, 10)
+
+    def test_customer_has_name(self):
+        self.assertEqual("Frodo", self.customer.name)
