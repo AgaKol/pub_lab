96525d34a21ada260e28b15cb85c1527000b9179
Aga Koltun <agnieszka.koltun.89@gmail.com>
2021-10-27 13:55:49 +0100

    test_customer_wallet_can_reduce added

--- src/customer.py
+++ src/customer.py
@@ -4,3 +4,6 @@ class Customer:
         self.age = age
         self.wallet = wallet
         self.drunkness = 0
+
+    def reduce_wallet(self, drink_price):
+        self.wallet -= drink_price
--- tests/customer_test.py
+++ tests/customer_test.py
@@ -1,10 +1,14 @@
 import unittest
 from src.customer import Customer
+from src.drinks import Drinks
+from src.pub import Pub
 
 
 class CustomerTest(unittest.TestCase):
     def setUp(self):
         self.customer = Customer("Frodo", 51, 10)
+        self.drink = Drinks("Beer", 1, 3)
+        self.pub = Pub("The Prancing Pony", 1000)
 
     def test_customer_has_name(self):
         self.assertEqual("Frodo", self.customer.name)
@@ -14,3 +18,12 @@ class CustomerTest(unittest.TestCase):
 
     def test_customer_has_wallet(self):
         self.assertEqual(10, self.customer.wallet)
+
+    # def test_customer_can_buy_drink(self):
+    #     self.pub.till.increase_till(self.drink.price)
+    #     self.customer.wallet.reduce_wallet(self.drink.price)
+    #     self.assertEqual(9, self.customer.buy_drink(self.drink.name))
+
+    def test_cutomer_wallet_can_decrease(self):
+        self.customer.reduce_wallet(self.drink.price)
+        self.assertEqual(9, self.customer.wallet)
